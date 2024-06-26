367b4cabc3695677e7911d563a7d342e4a4e613c
mgupta27 <mihir_gupta@rocketmail.com>
2024-06-25 22:26:24 -0400

    Initialized the block list

--- ece350_start/Core/Inc/common.h
+++ ece350_start/Core/Inc/common.h
@@ -13,7 +13,7 @@
 
 #include <stdint.h>
 
-//#define DEBUG_ENABLE // Comment me out to disable debugging
+#define DEBUG_ENABLE // Comment me out to disable debugging
 
 #ifdef DEBUG_ENABLE
 	#define DEBUG_PRINTF(fmt, ...) printf("DEBUG_PRINTF<<" fmt, ##__VA_ARGS__)
@@ -101,12 +101,13 @@ typedef struct Block {
 	uint32_t type; // FREE/USED
 	uint32_t size; // Block size including sizeof(Block)
 	uint32_t TIDofOwner;
+	uint32_t startingAddress;
 	struct Block* next; // Points to the start of the next block
 } Block;
 
 typedef struct BuddyHeap {
 	U32 currentBlockListIndex;
-	Block* blockList[NUMBER_OF_NODES]; // Store pointers to every block (may make life easier for accessing for re-ordering heap on the heap (heh))
+	Block blockList[NUMBER_OF_NODES]; // Store pointers to every block (may make life easier for accessing for re-ordering heap on the heap (heh))
 	Block* freeList[HEIGHT_OF_TREE]; // Eg, 2^5 = 32, 2^6 = 64, ....
 	U32 bitArray[NUMBER_OF_NODES];
 } BuddyHeap;
@@ -115,6 +116,7 @@ extern Kernel_Variables kernelVariables;
 extern BuddyHeap buddyHeap;
 extern uint32_t _img_end;
 extern uint32_t _estack;
+extern uint32_t _Min_Stack_Size;
 /**
  * @brief: Returns the MSP address
  */
--- ece350_start/Core/Src/k_mem.c
+++ ece350_start/Core/Src/k_mem.c
@@ -26,8 +26,8 @@ BuddyHeap buddyHeap;
 void osKernelInit(void) {
 	osInitTCBArray();
 	kernelVariables.kernelInitRan = 1;
-	kernelVariables.endOfHeap = (unsigned int)&_estack - MAX_STACK_SIZE;
 	kernelVariables.startOfHeap = (unsigned int)&_img_end;
+	kernelVariables.endOfHeap = (unsigned int)&_estack - (unsigned int)&_Min_Stack_Size;
 	return;
 }
 
@@ -60,7 +60,7 @@ void osInitTCBArray(void) {
 }
 
 int k_mem_init(void) {
-	if (kernelVariables.buddyHeapInit || kernelVariables.kernelInitRan)
+	if (kernelVariables.buddyHeapInit || !kernelVariables.kernelInitRan)
 		return RTX_ERR;
 
 	osInitBuddyHeap();
@@ -81,7 +81,19 @@ void osInitBuddyHeap(void) {
 	}
 
 	for (int i = 0; i < NUMBER_OF_NODES; i++) {
-		buddyHeap.blockList[i] = NULL;
+		if (i == 0) {
+			buddyHeap.blockList[i].type = FREE;
+			buddyHeap.blockList[i].size = kernelVariables.startOfHeap - kernelVariables.endOfHeap;
+			buddyHeap.blockList[i].TIDofOwner = TID_NULL;
+			buddyHeap.blockList[i].startingAddress = kernelVariables.startOfHeap;
+			buddyHeap.blockList[i].next = NULL;
+		} else {
+			buddyHeap.blockList[i].type = FREE;
+			buddyHeap.blockList[i].size = 0;
+			buddyHeap.blockList[i].TIDofOwner = TID_NULL;
+			buddyHeap.blockList[i].startingAddress = 0;
+			buddyHeap.blockList[i].next = NULL;
+		}
 	}
 }
 
